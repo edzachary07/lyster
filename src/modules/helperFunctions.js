6ca413e7ee6619bb997958480f43ce081e9d73ad
edzachary07 <edzachary007@gmail.com>
2026-04-02 23:07:57 +0000

    Deploying to gh-pages from @ edzachary07/lyster@e71141a2afedf47f712d35dc3ac0adac0e28d2a4 🚀

--- src/modules/helperFunctions.js
+++
@@ -1,9 +0,0 @@
-function generateRandomKey() {
-   let key = '';
-   for (let i=0; i<6; i++) {
-      key += `${Math.floor(Math.random() *10)}`;
-   }
-   return key;
-}
-
-export default generateRandomKey;
