bd7c15172453b67be23aa8be30ef2761a2c9ecbd
Zac Bingham <edzachary007@gmail.com>
2026-04-03 13:49:28 -0400

    mk20

--- src/modules/spotifyAccess.js
+++ src/modules/spotifyAccess.js
@@ -1,19 +1,30 @@
-async function accessSpotify() {
-   try {
-      const response = await fetch('https://accounts.spotify.com/authorize?' + JSON.stringify({
-         response_type: 'code',
-         client_id: '18292b93831e49c6a52ad6b5291159c7',
-         scope: 'user-read-private user-read-email',
-         redirect_uri: 'https://lyster.netlify.app/callback'
-    }));
-    if (response.ok) {
-      const jsonResponse = await response.json();
+// async function accessSpotify() {
+//    try {
+//       const response = await fetch('https://accounts.spotify.com/authorize?' + JSON.stringify({
+//          response_type: 'code',
+//          client_id: '18292b93831e49c6a52ad6b5291159c7',
+//          scope: 'user-read-private user-read-email',
+//          redirect_uri: 'https://lyster.netlify.app/callback'
+//     }));
+//     if (response.ok) {
+//       const jsonResponse = await response.json();
 
-    }
-    throw new Error('Request failed');
-   } catch (error) {
-      console.log(error);
-   }
+//     }
+//     throw new Error('Request failed');
+//    } catch (error) {
+//       console.log(error);
+//    }
+// }
+
+function accessSpotify() {
+   const params = new URLSearchParams({
+      response_type: 'code',
+      client_id: '18292b93831e49c6a52ad6b5291159c7',
+      scope: 'user-read-private user-read-email',
+      redirect_uri: 'https://lyster.netlify.app/callback'
+   });
+
+   window.location.href = `https://accounts.spotify.com/authorize?${params.toString()}`;
 }
 
 export default accessSpotify;
